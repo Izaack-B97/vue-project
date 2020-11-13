c441738f4b2d425211a2d8f720049b3131ae0787
Isaac Bustamante <izaack.97@gmail.com>
2020-11-13 08:52:41 -0700

    Components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Header></Header>
+    <Slider></Slider>
+    <div class="center">
+      <LastArticles></LastArticles>
+      <router-view></router-view>
+      <Sidebar></Sidebar>
+      <div class="clearfix"></div>
+      <FooterComponent></FooterComponent>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/HeaderComponent.vue';
+import Slider from './components/Slider';
+import Sidebar from './components/Sidebar.vue';
+import LastArticles from './components/LastArticles';
+import FooterComponent from './components/FooterComponet';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+    Header,
+    Slider,
+    Sidebar,
+    LastArticles,
+    FooterComponent
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
+
+@import './assets/css/styles.css';
 </style>
