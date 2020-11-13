f2c4d90ee7ef8cadb8e21a03874010e08f4de672
Isaac Bustamante <izaack.97@gmail.com>
2020-11-13 09:53:39 -0700

    directiva router-link

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import VueRouter from 'vue-router'
 import LastArticles from './components/LastArticles.vue'
 import MiComponente from './components/MiComponente.vue'
 import HolaMundo from './components/HelloWorld.vue'
+import Blog from './components/Blog.vue'
+import Formulario from './components/Formulario.vue'
+import Pagina from './components/Pagina.vue'
+
 
 Vue.config.productionTip = false
 
@@ -11,6 +15,9 @@ Vue.use(VueRouter);
 
 const routes = [
   { path: '/home', component: LastArticles },
+  { path: '/blog', component: Blog },
+  { path: '/formulario', component: Formulario },
+  { path: '/pagina', component: Pagina },
   { path: '/ultimos-articulos', component: LastArticles },
   { path: '/mi-componente', component: MiComponente },
   { path: '/', component: LastArticles },
