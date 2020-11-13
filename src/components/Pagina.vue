8cd0c72993018214aafba876599c543704c7042a
Isaac Bustamante <izaack.97@gmail.com>
2020-11-13 10:28:57 -0700

    Metodos de vida de un componente

--- src/components/Pagina.vue
+++ src/components/Pagina.vue
@@ -2,19 +2,44 @@
     <section id="content">
         <h2 class="subheader">P&aacute;gina de prueba</h2>
         <h2>{{ id_desde_url }}</h2>
+        <button @click="actualizarNombre()">Actualizar Nombre</button>
+        <h4>{{ nombre }}</h4>
     </section>
 </template>
 
 <script>
 export default {
     name: 'Pagina',
-    mounted() { // Se ejecuta al cargar el componente
-        this.id_desde_url = this.$route.params.id;
+    created() {
+        console.log('Componente creado');
     },
+
+    mounted() { // Se ejecuta al montar el componentge
+        this.id_desde_url = this.$route.params.id;
+        console.log('Componente montado');
+    },
+
+    updated() {
+        console.log('Componente actualizado');
+    },
+
+    destroyed() {
+        console.log('Componente eliminado');
+    },
+
     data() {
         return {
-            id_desde_url: null
+            id_desde_url: null,
+            nombre: 'Isaac Bustamante'
         };
-    }
+    },
+
+    // Aqui van mis metodos personalizados
+    methods: {
+        actualizarNombre() {
+            this.nombre = 'Ararat Ibarrola';
+            console.log(this.nombre);
+        }
+    } 
 }
 </script>
