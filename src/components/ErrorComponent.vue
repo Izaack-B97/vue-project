63b4dc61e0bfb0155b0807bc20d3f84c9f240021
Isaac Bustamante <izaack.97@gmail.com>
2020-11-19 08:56:26 -0700

    directiva active-class

--- src/components/ErrorComponent.vue
+++ src/components/ErrorComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <section id="content">
         <h1>P&aacute;gina no encontrada</h1>
-        <h2>La p&aacute;gina no fue encontrada intente m&aacute;s tarde.</h2>
+        <h2>La p&aacute;gina no fue encontrada, intente m&aacute;s tarde.</h2>
     </section>
 </template>
 
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -14,19 +14,19 @@
                 <nav id="menu">
                     <ul>
                         <li>
-                            <router-link to='/home'>Inicio</router-link>
+                            <router-link to='/home' active-class="active">Inicio</router-link>
                         </li>
                         <li>
-                            <router-link to='/blog'>Blog</router-link>
+                            <router-link to='/blog' active-class="active">Blog</router-link>
                         </li>
                         <li>
-                            <router-link to="/formulario">Formulario</router-link>
+                            <router-link to="/formulario" active-class="active">Formulario</router-link>
                         </li>
                         <li>
-                            <router-link :to="{ name: 'pagina', params: {id: 'Soy el parametro de la pagina'} }">Pagina 1</router-link>
+                            <router-link :to="{ name: 'pagina', params: {id: 'Soy el parametro de la pagina'} }" active-class="active">Pagina 1</router-link>
                         </li>
                         <li>
-                            <router-link to="/mi-componente">Pagina 2</router-link>
+                            <router-link to="/mi-componente" active-class="active">Pagina 2</router-link>
                         </li>                            
                     </ul>
                 </nav>
